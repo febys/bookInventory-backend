12c2fa3facfb29d5ab2c64ee6b80ad56d0cb22b6
febys <napsterfly@gmail.com>
2021-01-29 02:02:47 +0100

    “msg”

--- src/config/google-config.enum.ts
+++ src/config/google-config.enum.ts
@@ -1,4 +1,4 @@
 export enum GoogleConfig {
-    CLIENT_ID = '556932462258-tvl35ac9c14pbc2kpftk1ahtckhtigmj.apps.googleusercontent.com',
-    CLIENT_SECRET = '96ERPI-PQ0xDtwTSjublAd4T'
+    CLIENT_ID = '608387110510-qgbubj06qqika855ivhnk6fqdpvrj1ha.apps.googleusercontent.com',
+    CLIENT_SECRET = 'C9n8jbGqvBvCXwtb3Ke1QX_J'
 }
